3108d09445c92877ea708afdcb5da404986931c5
ChiquitoS11 <jooooshkk6@outlook.com>
2024-10-09 04:07:32 +0200

    dios soy yo otraves x3

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,11 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: 25060,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 db.connect(err => {
